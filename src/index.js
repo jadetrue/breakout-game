2ed01132e607c84ceecc61240cb718fb79c169ab
Jade <jadetruexo@gmail.com>
2021-06-25 14:01:15 +0100

    Added in the ball and paddle movement and updated it so there's a stopping point

--- src/index.js
+++ src/index.js
@@ -1,20 +1,90 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext('2d');
+// Ball
+let ballRadius = 10;
+// Ball positions
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+// Paddle height and width
+let paddleHeight = 10;
+let paddleWidth = 75;
+// Starting point
+let paddleX = (canvas.width-paddleWidth) / 2;
+// Paddle control
+let rightButtonPress = false;
+let leftButtonPress = false;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightButtonPress = true;
+    } else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftButtonPress = true;
+    }
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightButtonPress = false;
+    } else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftButtonPress = false;
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#eac5d8";
+    ctx.fill();
+    ctx.closePath;
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#ecebf5";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    if(rightButtonPress) {
+        paddleX += 6;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if(leftButtonPress) {
+        paddleX -= 6;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
+
+    x += dx;
+    y += dy;
+}
+
+let interval = setInterval(draw, 10);
